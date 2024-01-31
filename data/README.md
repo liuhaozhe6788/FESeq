80306c2d40a44f07a26474cafb728f6ccfa072b5
lhzstar <2792382045@qq.com>
2024-01-31 12:11:40 +0800

    new commits

--- data/README.md
+++
@@ -1,4 +0,0 @@
-## Datasets
-
-A list of benchmark datasets for CTR prediction are available at https://openbenchmark.github.io/BARS/datasets/README.html#
-
--- run_widedeep.sh
+++ run_widedeep.sh
@@ -6,7 +6,9 @@ echo ${PYTHONPATH}
 
 cd $home/WideDeep
 
-echo "=== Training WideDeep_torch ===" && python run_expid.py --gpu 0 --expid WideDeep_eleme
+# echo "=== Training WideDeep_eleme ===" && python run_expid.py --gpu 0 --expid WideDeep_eleme
 
-echo "=== Training WideDeep_torch ===" && python run_expid.py --gpu 0 --expid WideDeep_bundle
+# echo "=== Training WideDeep_bundle ===" && python run_expid.py --gpu 0 --expid WideDeep_bundle
+
+echo "=== Training WideDeep_ml10m ===" && python run_expid.py --gpu 0 --expid WideDeep_ml10m
 
