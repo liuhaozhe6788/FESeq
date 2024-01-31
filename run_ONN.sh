af3f779271a261e4952d56281271d514a11e51cc
lhzstar <2792382045@qq.com>
2024-01-31 19:30:40 +0800

    new commits

--- run_ONN.sh
+++ run_ONN.sh
@@ -1,10 +1,13 @@
 #! /bin/sh
+pip install -r requirements.txt
 home="model_zoo"
 
 export PYTHONPATH=${PWD}
 echo ${PYTHONPATH}
 cd $home/ONN/ONN_torch 
 
-echo "=== Training ONNv2 ===" && python run_expid.py --gpu 0 --expid ONNv2_eleme
+echo "=== Training ONNv2_eleme ===" && python run_expid.py --gpu 0 --expid ONNv2_eleme
 
-echo "=== Training ONNv2 ===" && python run_expid.py --gpu 0 --expid ONNv2_bundle
+echo "=== Training ONNv2_bundle ===" && python run_expid.py --gpu 0 --expid ONNv2_bundle
+
+echo "=== Training ONNv2_ml10m ===" && python run_expid.py --gpu 0 --expid ONNv2_ml10m
